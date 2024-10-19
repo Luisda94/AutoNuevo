b7c1fdbec7d9a53710d9d92bf723fc40b19baec7
Luisda <luisda915@gmail.com>
2024-10-18 21:46:15 -0300

    Se crean atributos

--- src/modelo/Auto.java
+++ src/modelo/Auto.java
@@ -1,5 +1,21 @@
 package modelo;
 
 public class Auto {
-
+/*Crearás una clase Auto con los siguientes atributos: marca, modelo, color, velocidadMaxima.
+Usted decide el tipo de dato que crea más conveniente. Agregará un cuarto atributo de tipo
+booleano, de nombre encendido. Los atributos no deben ser accesibles desde otra clase.
+Crearás los métodos de encendido del auto, apagado y acelerado del mismo. Debe evaluar que,
+si el auto está apagado no pueda acelerar, si se encuentra encendido no se pueda encender otra
+vez, si está apagado no se vuelva a apagar.
+Deberás crear un menú́donde podrás elegir lo que quieras realizar. El menú no se detiene hasta
+que usted decida salir. Para producir el objeto solicitarás que los datos sean ingresados desde
+la consola. Una vez generado imprimirá los datos. */
+	
+	   private String marca;
+	    private String modelo;
+	    private String color;
+	    private int velocidadMaxima;
+	    private boolean encendido;
+	    
+	    
 }
